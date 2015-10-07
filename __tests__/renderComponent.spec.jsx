8ad9741a5a3ee9e009187fec46fb3f25643a7ea4
Jack Callister <jack.callister@quillcontent.com>
2015-10-07 10:19:14 +0100

    Add context and deeply nested component specs.

--- __tests__/renderComponent.spec.jsx
+++ __tests__/renderComponent.spec.jsx
@@ -1,11 +1,65 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import assert from 'assert'
-import { renderComponent } from '../lib/react-test-unit'
+import { renderComponent } from '../src/index'
 
 class SingleChildComponent extends Component {
 
   render() {
     return <h1>Test</h1>
+  }
+}
+
+class NestedComponent extends Component {
+  render() {
+    return (
+      <div>
+        <InnerComponent prop='Test Prop' />
+      </div>
+    )
+  }
+}
+
+class InnerComponent extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.prop}
+      </div>
+    )
+  }
+}
+
+class ContextComponent extends Component {
+
+  static contextTypes = {
+    prop: PropTypes.string
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          {this.context.prop}
+        </div>
+
+        <InnerContextComponent />
+      </div>
+    )
+  }
+}
+
+class InnerContextComponent extends Component {
+
+  static contextTypes = {
+    prop: PropTypes.string
+  }
+
+  render() {
+    return (
+      <div>
+        {this.context.prop}
+      </div>
+    )
   }
 }
 
@@ -46,15 +100,35 @@ class OtherComponent extends Component {
 describe('renderComponent', () => {
 
   context('with a SingleChildComponent', () => {
-    const component = renderComponent(<SingleChildComponent />)
+    const component = renderComponent(SingleChildComponent)
 
     it('should have the text as the child', () => {
       assert.equal('Test', component.props.children)
     })
   })
 
+  context('with a NestedComponent', () => {
+    const component = renderComponent(NestedComponent)
+
+    it('should render the nested component with props', () => {
+      assert.equal('Test Prop', component.props.children.props.children)
+    })
+  })
+
+  context('with a ContextComponent', () => {
+    const component = renderComponent(ContextComponent, {}, { prop: 'Test Context' })
+
+    it('should have context props', () => {
+      assert.equal('Test Context', component.props.children[0].props.children)
+    })
+
+    it('should pass context down', () => {
+      assert.equal('Test Context', component.props.children[1].props.children)
+    })
+  })
+
   context('with a MultipleElementChildrenComponent', () => {
-    const component = renderComponent(<MultipleElementChildrenComponent />)
+    const component = renderComponent(MultipleElementChildrenComponent)
 
     it('should be a div', () => {
       assert.equal('div', component.type)
@@ -76,7 +150,7 @@ describe('renderComponent', () => {
   })
 
   context('with a ComponentComponent', () => {
-    const component = renderComponent(<ComponentComponent />)
+    const component = renderComponent(ComponentComponent)
 
     it('should be a div', () => {
       assert.equal('div', component.type)
